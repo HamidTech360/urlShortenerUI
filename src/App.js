13edbab720130287d1dc30e4ce04399889209876
HamidTech360 <hammedowolabi2001@gmail.com>
2021-12-30 11:04:53 -0800

    adding the post page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/home/home';
+import Post from './pages/post/post'
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-      
+        <Route path="/post" component={Post}/>
         <Route path="/" component={Home}/>
       </Switch>
       
---
+++ src/pages/post/components/comments.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../css/comment.css'
+
+const Comment =()=>{
+    return (
+        <div className="comment">
+            <div className="comment-title">5 Comments</div>
+            <div className="comment-tab">
+                <div className="comment-name">Hamid</div>
+                <div className="comment-date">25th july, 2022</div>
+                <div className="comment-text">Nice post there. That was an awesome one to go with fellas</div>
+            </div>
+        </div>
+    )
+}
+
+export default Comment
